6c4b2186ba5282b3ac69bdeb7b6c304c4ce6bac4
Juanita003 <juanacosato@gmail.com>
2025-05-26 21:58:29 -0500

    Corrección server

--- datanode_grpc/server.py
+++ datanode_grpc/server.py
@@ -2,6 +2,8 @@ import grpc
 from concurrent import futures
 import os
 import sys
+import requests
+
 
 # Permitir importar el paquete common
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -10,6 +12,22 @@ from common.config import grpc_base_port
 from services.grpc_service import DataNodeGRPCService
 import protos.datanode_pb2_grpc as datanode_pb2_grpc
 import protos.datanode_pb2      as datanode_pb2
+
+def registrar_en_namenode(node_id, port):
+    namenode_url = "http://34.201.251.107:8080</api/datanodes/registro"
+    payload = {
+        "node_id": node_id,
+        "host": "34.201.251.107",
+        "port": port
+    }
+    try:
+        response = requests.post(namenode_url, json=payload)
+        if response.status_code == 200:
+            print("✅ DataNode registrado en NameNode")
+        else:
+            print(f"❌ Fallo en el registro del DataNode: {response.text}")
+    except Exception as e:
+        print(f"❌ Error conectando al NameNode: {e}")
 
 
 def serve(node_id: int, storage_dir=None):
